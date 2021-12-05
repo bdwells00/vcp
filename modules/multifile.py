22b4420df8c081343a8f3853ac57d46de17375af
bdwells00 <wellsb.droid@gmail.com>
2021-12-05 10:53:17 -0500

    v0.7.5 - refactoring and code cleanup

--- modules/multifile.py
+++ modules/multifile.py
@@ -23,22 +23,20 @@ def file_write(file_handle, file_blocks):
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 @mono_timer
-def hex_processing(hash_action, file_blocks=0):
-    # hashlib function variable
-    hlib_var = (getattr(hashlib, args.hash)())
+def hash_processing(hlib, hash_action, file_blocks=0):
     if hash_action == 'update':
-        hlib_var.update(file_blocks)
-        return
+        hash_upd = hlib.update(file_blocks)
+        return hash_upd
     elif hash_action == 'hex':
         # convert the hash to hexadecimal
         if 'shake' in args.hash:
-            return hlib_var.hexdigest(args.length)
+            return hlib.hexdigest(args.length)
         else:
-            return hlib_var.hexdigest()
+            return hlib.hexdigest()
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-def file_multifunction(file_source: str, file_action: str):
+def file_multi(file_action: str, file_source: str):
     """Copy or read the file with hashing.
 
     Args:
@@ -54,6 +52,8 @@ def file_multifunction(file_source: str, file_action: str):
             - file_target     the target file name
             - file_size       the size of the source file
     """
+    # hashlib function variable
+    hlib_var = (getattr(hashlib, args.hash)())
     # swap the target for the source in the string
     file_target = file_source.replace(args.source, args.target)
     file_rtime, file_wtime, file_size, hash_time = 0, 0, 0, 0
@@ -79,7 +79,7 @@ def file_multifunction(file_source: str, file_action: str):
                         print()
                     break
                 # update the hash on each chunk
-                hash_temp = hex_processing('update', f_chunk[2])
+                hash_temp = hash_processing(hlib_var, 'update', f_chunk[2])
                 hash_time += hash_temp[1]
                 # skip this section on read hashing, otherwise copy the file
                 if file_action == 'copy':
@@ -92,7 +92,7 @@ def file_multifunction(file_source: str, file_action: str):
                         Ct.BBLUE, ' | ', Ct.A, f'{file_source}', Ct.GREEN],
                         inl=1, num=0, fls=1)
             bp(['', Ct.A])
-            hash_temp = hex_processing('hex')
+            hash_temp = hash_processing(hlib_var, 'hex')
             hash_time += hash_temp[1]
 
         return (0, file_rtime, file_wtime, hash_time, hash_temp[2],
@@ -103,3 +103,71 @@ def file_multifunction(file_source: str, file_action: str):
         bp([f'{e_var}', Ct.RED], erl=2)
 
         return 1, file_source
+
+
+def file_logic(file_list):
+    # variables for file processing
+    file_dict = {
+        'success': 0,
+        'success_source_list': [],
+        'success_target_list': [],
+        'failure': 0,
+        'failure_list': [],
+        'read_time': 0.0,
+        'write_time': 0.0,
+        'hash_time': 0.0,
+        'hash_source_list': [],
+        'read_size': 0,
+        'val_success': 0,
+        'val_success_list': [],
+        'val_failure': 0,
+        'val_failure_list': [],
+        'val_read_time': 0.0,
+        'val_hash_time': 0.0,
+        'val_hash_list': [],
+        'val_size': 0
+    }
+    # copy each file and validate it with stats
+    for file in file_list:
+        # execute file creation and hashing, and gather stats
+        copy_return = file_multi('copy', file)
+        if copy_return[0] == 0:
+            file_dict['success'] += 1
+            file_dict['success_source_list'].append(file)
+            file_dict['read_time'] += copy_return[1]
+            file_dict['write_time'] += copy_return[2]
+            file_dict['hash_time'] += copy_return[3]
+            file_dict['hash_source_list'].append(copy_return[4])
+            file_dict['success_target_list'].append(copy_return[5])
+            file_dict['read_size'] += copy_return[6]
+            bp([f'Copied: {file_dict["success_source_list"][-1]}', Ct.GREEN],
+                num=0, veb=1)
+        else:
+            file_dict['failure'] += 1
+            file_dict['failure_list'].append(file)
+            bp([f'Failed Copy!: {file_dict["failure_list"][-1]}', Ct.RED],
+                erl=2)
+        # execute target file read and hashing, and gather stats
+        val_return = file_multi('read', copy_return[5])
+        if copy_return[0] == 0 and val_return[0] == 0:
+            file_dict['val_read_time'] += val_return[1]
+            file_dict['val_hash_time'] += val_return[3]
+            file_dict['val_hash_list'].append(val_return[4])
+            if copy_return[4] == val_return[4]:
+                file_dict['val_success'] += 1
+                file_dict['val_success_list'].append(val_return[5])
+                file_dict['val_size'] += val_return[6]
+                bp([f'Validated: source & target hex match.\n\t'
+                    f'{copy_return[4]}\n\t{val_return[4]}', Ct.GREEN], num=0,
+                    veb=1)
+            else:
+                file_dict['val_failure'] += 1
+                file_dict['val_failure_list'].append(val_return[5])
+                bp([f'Source & target hex DO NOT MATCH!\n\t{copy_return[4]}'
+                    f'\n\t{val_return[4]}', Ct.RED], erl=2, num=0)
+        else:
+            file_dict['val_failure'] += 1
+            file_dict['val_failure_list'].append(copy_return[5])
+            bp([f'Failed reading copied file!: {copy_return[5]}', Ct.RED],
+                erl=2)
+    return file_dict
--- vcp.py
+++ vcp.py
@@ -9,7 +9,7 @@ from modules.bp import bp
 from modules.bytenote import byte_notation
 from modules.createfolder import folder_logic
 from modules.ct import Ct
-from modules.multifile import file_multifunction
+from modules.multifile import file_logic
 import modules.options as options
 import modules.treewalk
 START_PROG_TIME = time.monotonic()
@@ -39,72 +39,41 @@ def main():
             f'{file_size_total[1]}', Ct.A])
 
         folder_return = folder_logic(tw_tup[1], args)
-        print(folder_return)
 
         f_time = folder_return[1]
         folder_time = f'{f_time:,.4f}'
         folder_success = folder_return[2][0]
         folder_failure = folder_return[2][1]
-        # variables for file processing
-        fc_rtime, fc_wtime, hc_time, fr_rtime, hr_time = 0, 0, 0, 0, 0
-        file_success, file_failure, file_size_copied = 0, 0, 0
-        val_success, val_failure = 0, 0
-        # copy each file and validate it with stats
-        # TODO add confirmation for file copy and read
-        for file in tw_tup[2]:
-            # execute file creation and hashing, and gather stats
-            c_stat = file_multifunction(file, 'copy')
-            if c_stat[0] == 0:
-                file_success += 1
-                fc_rtime += c_stat[1]
-                fc_wtime += c_stat[2]
-                hc_time += c_stat[3]
-                f_target = c_stat[5]
-                file_size_copied += c_stat[6]
-                bp([f'Copied: {c_stat[5]}', Ct.GREEN], num=0)
-            else:
-                file_failure += 1
-                bp([f'Failed Copy!: {c_stat[1]}', Ct.RED], erl=2)
-            # execute target file read and hashing, and gather stats
-            r_stat = file_multifunction(f_target, 'read')
-            if c_stat[0] == 0 and r_stat[0] == 0:
-                fr_rtime += r_stat[1]
-                hr_time += r_stat[2]
-                if c_stat[4] == r_stat[4]:
-                    val_success += 1
-                    bp([f'Validated: source & target hex match.\n\t{c_stat[4]}'
-                        f'\n\t{r_stat[4]}', Ct.GREEN], num=0)
-                else:
-                    val_failure += 1
-                    bp([f'Source & target hex DO NOT MATCH!\n\t{c_stat[4]}\n\t'
-                        f'{r_stat[4]}', Ct.RED], erl=2, num=0)
-            else:
-                bp([f'Failed reading copied file!: {c_stat[1]}', Ct.RED],
-                   erl=2)
+
+        file_return = file_logic(tw_tup[2])
 
         # print out final copy stats
-        file_size_success = byte_notation(file_size_copied)
-        file_size_failure = byte_notation((tw_tup[3] - file_size_copied))
-        hex_tot = hc_time + hr_time
-        file_tot = int(fc_rtime + fc_wtime)
+        file_size_success = byte_notation(file_return["val_size"])
+        file_size_failure = byte_notation(tw_tup[3] - file_return["val_size"])
+        hex_tot = file_return["hash_time"] + file_return["val_hash_time"]
+        file_tot = int(file_return['read_time'] + file_return["write_time"])
         bp([f'\n{" " * 16}Source    Target    FAILED         TIME', Ct.A])
         bp([f'      Dirs: {folder_total:>10}{folder_success:>10}'
             f'{folder_failure:>10,}{folder_time:>12s}s', Ct.A])
-        bp([f'     Files: {file_total:>10}{file_success:>10}'
-           f'{file_failure:>10,}{file_tot:>12,.4f}s', Ct.A])
+        bp([f'     Files: {file_total:>10}{file_return["success"]:>10}'
+           f'{file_return["failure"]:>10,}{file_tot:>12,.4f}s', Ct.A])
         bp([f'     Bytes: {file_size_total[1]:>10}{file_size_success[1]:>10}'
            f'{file_size_failure[1]:>10}', Ct.A])
-        bp([f'Validation: {file_total:>10}{val_success:>10}'
-           f'{val_failure:>10,}{hex_tot:>12,.4f}s (+{fr_rtime:,.4f}s)', Ct.A])
+        bp([f'Validation: {file_total:>10}{file_return["val_success"]:>10}'
+            f'{file_return["val_failure"]:>10,}{hex_tot:>12,.4f}s (+'
+            f'{file_return["val_read_time"]:,.4f}s)', Ct.A])
         end_time = time.monotonic()
         total_time = end_time - START_PROG_TIME
-        tft = (tw_timer[1] + f_time + fc_rtime + hc_time + fc_wtime +
-               fr_rtime + hr_time)
-        bp([f'\n{total_time:,.4f}s - Total Time\n{tw_timer[1]:,.4f}s - Tree Wa'
-            f'lk Time\n{folder_time:}s - FolderCreation Time\n{fc_rtime:,.4f}s'
-            f' - Source Read Time\n{hc_time:,.4f}s - Source Hash Validation '
-            f'Time\n{fc_wtime:,.4f}s - Target Write Time\n{fr_rtime:,.4f}s - '
-            f'Target Read Time\n{hr_time:,.4f}s - Target Hash Validation '
+        tft = (tw_timer[1] + f_time + file_return["read_time"] +
+               file_return["hash_time"] + file_return["write_time"] +
+               file_return["val_read_time"] + file_return["val_hash_time"])
+        bp([f'\n{total_time:,.4f}s - Total Time\n{tw_timer[1]:,.4f}s - Tree '
+            f'Walk Time\n{folder_time:}s - FolderCreation Time\n'
+            f'{file_return["read_time"]:,.4f}s - Source Read Time\n'
+            f'{file_return["hash_time"]:,.4f}s - Source Hash Validation Time\n'
+            f'{file_return["write_time"]:,.4f}s - Target Write Time\n'
+            f'{file_return["val_read_time"]:,.4f}s - Target Read Time\n'
+            f'{file_return["val_hash_time"]:,.4f}s - Target Hash Validation '
             f'Time\n{tft:,.4f}s - Total Function Time\n{"-" * 40}\n'
             f'{total_time - tft:,.4f}s - Program Overhead Time', Ct.A])
 
